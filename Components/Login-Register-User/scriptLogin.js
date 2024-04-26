fc437acfcbbb1acf1d40520a3dd1dac6a6d4311e
sebasp-125 <sp6027193@gmail.com>
2024-04-25 22:33:15 -0600

    Profile 60%

--- Components/Login-Register-User/scriptLogin.js
+++ Components/Login-Register-User/scriptLogin.js
@@ -1,7 +1,9 @@
+// Función para retroceder en la historia del navegador
 document.getElementById("Back").addEventListener("click", function () {
     window.history.back();
 });
 
+// Redirección basada en el tipo de usuario seleccionado
 document.getElementById("user_type").addEventListener("change", function () {
     let userType = this.value;
     console.log(userType);
@@ -9,29 +11,55 @@ document.getElementById("user_type").addEventListener("change", function () {
         window.location.href = "../LoginRegister-Farmer/Login-Farmer.html";
     }
 });
-//Verification Use
+
+// Verificación de credenciales al enviar el formulario
 document.querySelector('form').addEventListener('submit', function (event) {
-    event.preventDefault()
+    event.preventDefault();
 
     let campoCorreo = document.getElementById('campoCorreo').value;
     let campoContraseña = document.getElementById('campoContraseña').value;
 
     axios.get('https://render-delcamp.onrender.com/clientes')
         .then((dataPost) => {
-            let flag = false
+            let flag = false;
+            let TimeReload = 5; // Establecer un valor inicial para TimeReload
+
             dataPost.data.forEach(element => {
-                if (element.Correo == campoCorreo && element.Contraseña == campoContraseña) {
-                    console.log('Ingresaste ' , element.id)
-                    flag = true
-                    window.location.href = '../leanding_page.html?IdUserLogin=' + element.id;
-                }
-                if (!flag) {
-                    console.log('No ingresaste')
+                if (element.Correo === campoCorreo && element.Contraseña === campoContraseña) {
+                    console.log('Ingresaste ', element.id);
+                    flag = true;
+                    let contador = 5; 
+                    
+                    let intervalo = setInterval(() => {
+                        contador--;
+                        document.getElementById('TrueFormulario').innerHTML = `
+                        <h3>Iniciando con Exito... ${contador}</h3>
+                    `;
+                        if (contador === 0) {
+                            clearInterval(intervalo);
+                            window.location.href = '../leanding_page.html?IdUserLogin=' + element.id;
+                        }
+                    }, 1000);
                 }
             });
-        })
-        .catch((epa) => {
-            console.log("Error ", epa)
-        })
 
-})
+            if (!flag) {
+                console.log('No ingresaste');
+                let interval = setInterval(() => {
+                    TimeReload--;
+                    if (TimeReload === 0) {
+                        clearInterval(interval);
+                        window.location.reload();
+                    } else {
+                        document.getElementById('TrueFormulario').innerHTML = `
+                            <h3>Correo o Contraseña Incorrecta</h3>
+                            <p>Recarga en ${TimeReload} segundos</p>
+                        `;
+                    }
+                }, 1000); // Actualizar cada segundo
+            }
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });
+});
--- Components/ScriptPerfilUSER.js
+++ Components/ScriptPerfilUSER.js
@@ -4,42 +4,49 @@ function obtenerDatosUsuario() {
     let ValidationId = BajarId.get('IdUserLogin');
 
     return axios.get(`https://render-delcamp.onrender.com/clientes/${ValidationId}`)
-        .then((Clientes) => {
-            return { DATA: Clientes.data }
+        .then((response) => {
+            return response.data;
         })
-        .catch((ErrorUser) => {
-            console.error("Error", ErrorUser);
+        .catch((error) => {
+            console.error("Error", error);
             const ErroTry = document.getElementById('ErroTry')
             ErroTry.innerHTML = ` 
-                <h1> Usuario no Iniciado Sesion</h1> ${ErrorUser.message}
+                <h1> Usuario no Iniciado Sesion</h1> ${error.message}
                 <button type="button" onclick="Volver(event)" class="btn btn-success">Volver</button>
-                `
+            `
         });
 }
 
-function Volver(event) {
+let GroupFunctions = {
+    UserLogin: {
+        LoginUser: function () {
+            window.location.href = '/Components/Login-Register-User/user_login.html'
+        },
+        RegisterUser: function () {
+            window.location.href = '/Components/Login-Register-User/user_register.html'
+        },
+        LandingPage: function () {
+            window.location.href = '/Components/leanding_page.html'
+        }
+    },
+};
+
+function DelateUser(event) {
     event.preventDefault();
-    window.location.href = '/Components/Login-Register-User/user_login.html'
+    obtenerDatosUsuario()
+        .then((user) => {
+            let SpecifyIdUser = user.id;
+            axios.delete(`https://render-delcamp.onrender.com/clientes/${SpecifyIdUser}`);
+            console.warn("removed success!!");
+            setTimeout(() => {
+                GroupFunctions.UserLogin.RegisterUser();
+            }, 2000);
+        })
+        .catch((err) => {
+            console.error("error in delate user ", err);
+        })
 }
 
-
-obtenerDatosUsuario()
-    .then((Cliente) => {
-        let ObjeArray = Object.values(Cliente)
-        const sectionInformationUser = document.getElementById('sectionInformationUser')
-        ObjeArray.forEach((User) => {
-            sectionInformationUser.innerHTML += ` 
-        <img class='ImagenProfileUser' src="https://cdn.discordapp.com/attachments/1219748133177655306/1232860477806088243/historiaDelCamp.png?ex=662afe4b&is=6629accb&hm=04565fc1481d1d2b88d92d5e77043cba9254067b496e522f5a45e86755ddc8e7&" class="img-thumbnail" alt="...">
-        <h2>${User.nombre}</h2>
-        <h4>${User.apellido}</h4>
-        <h4>${User.Correo}</h4>
-        <h4>${User.apellido}</h4>
-        <button type="button" class="btn btn-success">Success</button>
-            `
-        })
-    });
-
-obtenerDatosUsuario();
 //Imagen de Perfil
 function cambiarFile() {
     const ImageProfileUser = document.getElementById('ImageProfileUser');
@@ -47,26 +54,31 @@ function cambiarFile() {
         const file = ImageProfileUser.files[0];
         const objectURL = URL.createObjectURL(file);
 
-        //------HERE------
-        localStorage.setItem("ImAgen", objectURL)
-        let imageProfileLOCAL = localStorage.getItem("ImAgen")
-        console.log(imageProfileLOCAL);
-        //------HERE------
+        localStorage.setItem("ImAgen", objectURL);
 
         document.getElementById('InformationImageProfile').innerHTML = `
-        <h4 class="titleInformationX">Information Image the Profile</h4>
-        <h5 class="NameImage">Name actually image: <h5 class="Result">${file.name}</h5>
-        <h5 class="NameImage">Type the Image: </h5> <h5 class="Result">${file.type}</h5>
-        <div class="container">
-
-        <img src="${imageProfileLOCAL}" alt="profile image user selection" srcset="">
-
-        <button class="btn btn-outline-success" type="submit">Subir</button>
-</div>
-
+            <h4 class="titleInformationX">Information Image the Profile</h4>
+            <h5 class="NameImage">Name actually image: <h5 class="Result">${file.name}</h5>
+            <h5 class="NameImage">Type the Image: </h5> <h5 class="Result">${file.type}</h5>
+            <div class="container">
+                <img src="${objectURL}" alt="profile image user selection" srcset="">
+                <button class="btn btn-outline-success" type="submit">Subir</button>
+            </div>
         `;
     }
 }
 
-//Aplly localStorange in the image profile and information user. for that when updoloand the page not delate information user 
-
+// Llamar a cambiarFile() cuando se selecciona un archivo
+document.getElementById('ImageProfileUser').addEventListener('change', cambiarFile);
+obtenerDatosUsuario()
+    .then((user) => {
+        let imageProfileLOCAL = localStorage.getItem("ImAgen");
+        const sectionInformationUser = document.getElementById('sectionInformationUser');
+        sectionInformationUser.innerHTML += ` 
+            <img class='ImagenProfileUser' src="${imageProfileLOCAL}" class="img-thumbnail" alt="...">
+            <h2>${user.nombre}</h2>
+            <h4>${user.apellido}</h4>
+            <h4>${user.Correo}</h4>
+            <button type="button" class="btn btn-success">Success</button>
+        `;
+    });
